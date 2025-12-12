d55593b6dc040a459ff51c7619ce53fafd84e9db
AourfiIdir <idir.aourfi@se.univ-bejaia.dz>
2025-12-12 18:17:54 -0500

    added files handler

--- include/huffman.h
+++ include/huffman.h
@@ -1,6 +1,8 @@
+
 #ifndef HUFFMAN_H
 #define HUFFMAN_H
 
+#include<raylib.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -26,10 +28,10 @@ struct MinHeap* createMinHeap(unsigned capacity);
 struct MinHeapNode* extractMin(struct MinHeap* minHeap);
 void insertMinHeap(struct MinHeap* minHeap, struct MinHeapNode* minHeapNode);
 void buildMinHeap(struct MinHeap* minHeap);
-void printArr(int arr[], int n) ;
+void printArr(int arr[], int n,FILE*f) ;
 int isLeaf(struct MinHeapNode* root) ;
-void printCodes(struct MinHeapNode* root, int arr[], int top) ;
-void HuffmanCodes(char data[], int freq[], int size);
+void printCodes(struct MinHeapNode* root, int arr[], int top,FILE*f) ;
+void HuffmanCodes(char data[], int freq[], int size,const char* filename);
 
 #endif
 
--- main.c
+++ main.c
@@ -1,11 +1,62 @@
-#include "include/huffman.h"
+#include"include/huffman.h"
+#define LOOPING(x,l) for(int x=0; x<(l); x++)
+
+typedef struct MapEntry{
+    char c;
+    int freq;
+} MapEntry;
+
+
+void getFreq(MapEntry map[26], char c, int *index){
+    
+    if (c == ' ' || c == '\n')
+        return;
+
+    for (int i = 0; i < *index; i++){
+        if (map[i].c == c){
+            map[i].freq++;
+            return;
+        }
+    }
+
+    if (*index < 26) {
+        map[*index].c = c;
+        map[*index].freq = 1;
+        (*index)++;
+    }
+}
 
 int main() {
-    //you can use a function to let the user select the array 
-    //or make it more advanced by using files directly :/
-    char arr[] = {'a', 'b', 'c', 'd', 'e'};//you can write a function that returns all the characters used
-    int freq[] = {5, 9, 12, 13, 16};//you can write a function that extracts the freq on a file
-    int size = sizeof(arr) / sizeof(arr[0]);
-    HuffmanCodes(arr, freq, size);
+
+    FILE *f = fopen("test.txt","r");
+    if (!f){
+        perror("cant open the file");
+        exit(1);
+    }
+
+    int c;
+    int index = 0;
+    MapEntry buffer[26] = {0};
+
+    while ((c = fgetc(f)) != EOF){
+        putchar(c);
+        getFreq(buffer, (char)c, &index);
+    }
+
+    LOOPING(i, index){
+        printf("char %c freq %d\n", buffer[i].c, buffer[i].freq);
+    }
+
+    char buffer2[26];
+    int fres[26];
+
+    for (int i = 0; i < index; i++){
+        buffer2[i] = buffer[i].c;
+        fres[i] = buffer[i].freq;
+    }
+
+    HuffmanCodes(buffer2, fres, index,"result.txt");
+
     return 0;
 }
+
